4984b8e8b6b465f2bc1c9aecceef87ea66ebb7d8
rudy <luhuadong@163.com>
2017-05-03 19:23:20 +0800

    modified /etc/rc.local & /opt/aec/ & /opt/resistive/gpio0_set.sh

--- rootfs/opt/resistive/gpio0_set.sh
+++ rootfs/opt/resistive/gpio0_set.sh
@@ -3,12 +3,12 @@
 # gpio0 --> touchscreen
 
 echo "Pin GPIO_0 RESET ..."
-echo out > /sys/class/gpio/gpio0/direction
-echo 0 > /sys/class/gpio/gpio0/value
+echo out > /sys/class/gpio/gpio64/direction
+echo 0 > /sys/class/gpio/gpio64/value
 #sleep 3	# eGTouch-v2.5.4330
 sleep 6		# eGTouch-v2.5.5814
 echo "Pin GPIO_0 SET ..."
-echo out > /sys/class/gpio/gpio0/direction
-echo 1 > /sys/class/gpio/gpio0/value
+echo out > /sys/class/gpio/gpio64/direction
+echo 1 > /sys/class/gpio/gpio64/value
 exit 0
 
